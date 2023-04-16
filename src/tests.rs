27c6063686c158bca3f8fdce405657be9b5b121e
Dorje Gilfillan <dorjegilfillan@gmail.com>
2023-04-16 05:29:42 +0200

    added subtraction

--- src/tests.rs
+++ src/tests.rs
@@ -6,101 +6,155 @@ use crate::{
     vm,
 };
 
-#[test]
-pub fn test_ints() {
-    let ints = [10, 15, 20, 25];
+mod load_literals {
+    use super::*;
 
-    let mut pool = Pool::default();
-    for int in ints {
-        pool.push_int(int);
+    #[test]
+    pub fn int() {
+        let ints = [10, 15, 20, 25];
+
+        let mut pool = Pool::default();
+        for int in ints {
+            pool.push_int(int);
+        }
+
+        let stack = vm::create_and_run(pool.as_bytes());
+        let expected_stack: Vec<Value> = ints.into_iter().map(Value::Int).collect();
+        assert_eq!(stack, expected_stack);
     }
 
-    let stack = vm::create_and_run(pool.as_bytes());
-    let expected_stack: Vec<Value> = ints.into_iter().map(Value::Int).collect();
-    assert_eq!(stack, expected_stack);
-}
+    #[test]
+    pub fn float() {
+        let floats = [1.1, 0.2, 12.2, f64::MAX, f64::INFINITY, f64::NEG_INFINITY];
 
-#[test]
-pub fn test_floats() {
-    let floats = [1.1, 0.2, 12.2, f64::MAX, f64::INFINITY, f64::NEG_INFINITY];
+        let mut pool = Pool::default();
+        for float in floats {
+            pool.push_float(float);
+        }
 
-    let mut pool = Pool::default();
-    for float in floats {
-        pool.push_float(float);
+        let stack = vm::create_and_run(pool.as_bytes());
+        let expected_stack: Vec<Value> = floats.into_iter().map(Value::Float).collect();
+        assert_eq!(stack, expected_stack);
     }
 
-    let stack = vm::create_and_run(pool.as_bytes());
-    let expected_stack: Vec<Value> = floats.into_iter().map(Value::Float).collect();
-    assert_eq!(stack, expected_stack);
+    #[test]
+    pub fn str() {
+        let strings = ["", "Hello, World!", &"repeated ".repeat(100)];
+
+        let mut pool = Pool::default();
+        for str in strings {
+            pool.push_str(str);
+        }
+
+        let stack = vm::create_and_run(pool.as_bytes());
+
+        let expected_stack: Vec<Value> = strings
+            .into_iter()
+            .map(|str| Value::Str(Cow::Borrowed(str)))
+            .collect();
+
+        assert_eq!(stack, expected_stack);
+    }
 }
+mod binop_add {
+    use super::*;
+    const OP: BinOp = BinOp::Add;
 
-#[test]
-pub fn test_strings() {
-    let strings = ["", "Hello, World!", &"repeated ".repeat(100)];
+    #[test]
+    pub fn int() {
+        let mut pool = Pool::default();
+        pool.push_int(1);
+        pool.push_int(2);
+        pool.push_binop(OP);
 
-    let mut pool = Pool::default();
-    for str in strings {
-        pool.push_str(str);
+        let stack = vm::create_and_run(pool.as_bytes());
+        assert_eq!(stack, vec![Value::Int(3)]);
     }
 
-    let stack = vm::create_and_run(pool.as_bytes());
+    #[test]
+    pub fn float() {
+        let mut pool = Pool::default();
+        pool.push_float(1.23);
+        pool.push_float(4.56);
 
-    let expected_stack: Vec<Value> = strings
-        .into_iter()
-        .map(|str| Value::Str(Cow::Borrowed(str)))
-        .collect();
+        pool.push_binop(OP);
 
-    assert_eq!(stack, expected_stack);
+        let stack = vm::create_and_run(pool.as_bytes());
+        assert_eq!(stack, vec![Value::Float(1.23 + 4.56)]);
+    }
+
+    #[test]
+    pub fn int_float() {
+        let mut pool = Pool::default();
+        pool.push_int(1);
+        pool.push_float(0.5);
+        pool.push_binop(OP);
+
+        pool.push_float(12.5);
+        pool.push_int(2);
+        pool.push_binop(OP);
+
+        let stack = vm::create_and_run(pool.as_bytes());
+        assert_eq!(
+            stack,
+            vec![Value::Float(1.0 + 0.5), Value::Float(12.5 + 2.0)]
+        );
+    }
+
+    #[test]
+    pub fn str() {
+        let mut pool = Pool::default();
+        pool.push_str("Hello, ");
+        pool.push_str("World!");
+        pool.push_binop(OP);
+
+        let stack = vm::create_and_run(pool.as_bytes());
+        assert_eq!(stack, vec![Value::Str(Cow::Owned("Hello, World!".into()))]);
+    }
 }
 
-#[test]
-pub fn test_int_add() {
-    let mut pool = Pool::default();
-    pool.push_int(1);
-    pool.push_int(2);
-    pool.push_binop(BinOp::Add);
+mod binop_sub {
+    const OP: BinOp = BinOp::Sub;
 
-    let stack = vm::create_and_run(pool.as_bytes());
-    assert_eq!(stack, vec![Value::Int(3)]);
-}
+    use super::*;
+    #[test]
+    pub fn int() {
+        let mut pool = Pool::default();
+        pool.push_int(1);
+        pool.push_int(2);
+        pool.push_binop(OP);
 
-#[test]
-pub fn test_float_add() {
-    let mut pool = Pool::default();
-    pool.push_float(1.23);
-    pool.push_float(4.56);
+        let stack = vm::create_and_run(pool.as_bytes());
+        assert_eq!(stack, vec![Value::Int(-1)]);
+    }
 
-    pool.push_binop(BinOp::Add);
+    #[test]
+    pub fn float() {
+        let mut pool = Pool::default();
+        pool.push_float(1.23);
+        pool.push_float(4.56);
 
-    let stack = vm::create_and_run(pool.as_bytes());
-    assert_eq!(stack, vec![Value::Float(1.23 + 4.56)]);
-}
+        pool.push_binop(OP);
 
-#[test]
-pub fn test_int_float_add() {
-    let mut pool = Pool::default();
-    pool.push_int(1);
-    pool.push_float(0.5);
-    pool.push_binop(BinOp::Add);
+        let stack = vm::create_and_run(pool.as_bytes());
+        assert_eq!(stack, vec![Value::Float(1.23 - 4.56)]);
+    }
 
-    pool.push_float(12.5);
-    pool.push_int(2);
-    pool.push_binop(BinOp::Add);
+    #[test]
+    pub fn int_float() {
+        let mut pool = Pool::default();
+        pool.push_int(1);
+        pool.push_float(0.5);
+        pool.push_binop(OP);
 
-    let stack = vm::create_and_run(pool.as_bytes());
-    assert_eq!(
-        stack,
-        vec![Value::Float(1.0 + 0.5), Value::Float(12.5 + 2.0)]
-    );
-}
+        pool.push_float(12.5);
+        pool.push_int(2);
+        pool.push_binop(OP);
 
-#[test]
-pub fn test_str_add() {
-    let mut pool = Pool::default();
-    pool.push_str("Hello, ");
-    pool.push_str("World!");
-    pool.push_binop(BinOp::Add);
-
-    let stack = vm::create_and_run(pool.as_bytes());
-    assert_eq!(stack, vec![Value::Str(Cow::Owned("Hello, World!".into()))]);
+        let stack = vm::create_and_run(pool.as_bytes());
+        assert_eq!(
+            stack,
+            vec![Value::Float(1.0 - 0.5), Value::Float(12.5 - 2.0)]
+        );
+    }
 }
--- src/value.rs
+++ src/value.rs
@@ -14,6 +14,7 @@ impl<'a> Value<'a> {
     pub fn run_binop(lhs: Self, rhs: Self, op: BinOp) -> Self {
         match op {
             BinOp::Add => Self::add(lhs, rhs),
+            BinOp::Sub => Self::sub(lhs, rhs),
             _ => todo!("{op:?}"),
         }
     }
@@ -23,12 +24,21 @@ impl<'a> Value<'a> {
         match (lhs, rhs) {
             (Self::Int(lhs), Self::Int(rhs)) => Self::Int(lhs + rhs),
             (Self::Float(lhs), Self::Float(rhs)) => Self::Float(lhs + rhs),
-            (Self::Float(lhs), Self::Int(rhs)) | (Self::Int(rhs), Self::Float(lhs)) => {
-                Self::Float(lhs + rhs as f64)
-            }
+            (Self::Float(lhs), Self::Int(rhs)) => Self::Float(lhs + rhs as f64),
+            (Self::Int(lhs), Self::Float(rhs)) => Self::Float(lhs as f64 + rhs),
             (Self::Str(lhs), Self::Str(rhs)) => {
                 Self::Str(Cow::Owned(lhs.into_owned() + rhs.as_ref()))
             }
+            (lhs, rhs) => todo!("{lhs:?} - {rhs:?}"),
+        }
+    }
+    #[allow(clippy::cast_precision_loss)]
+    fn sub(lhs: Self, rhs: Self) -> Self {
+        match (lhs, rhs) {
+            (Self::Int(lhs), Self::Int(rhs)) => Self::Int(lhs - rhs),
+            (Self::Int(lhs), Self::Float(rhs)) => Self::Float(lhs as f64 - rhs),
+            (Self::Float(lhs), Self::Int(rhs)) => Self::Float(lhs - rhs as f64),
+            (Self::Float(lhs), Self::Float(rhs)) => Self::Float(lhs - rhs),
             (lhs, rhs) => todo!("{lhs:?} - {rhs:?}"),
         }
     }
